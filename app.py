8caa20213c3d61f01d0ac15d7e01e94d42cddb06
21102 <88994587+21102@users.noreply.github.com>
2021-11-11 13:59:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -148,11 +148,11 @@ def event_handle(event):
         return ''
 
     if msgType == "text":
-        msg = str(event["message"]["text"])
+       msg = str(event["message"]["text"])
         if (msg == "สวัสดี") :
         replyObj = TextSendMessage(text="ดีด้วย")
         else :
-    line_bot_api.reply_message(rtoken, replyObj)
+        line_bot_api.reply_message(rtoken, replyObj)
     elif msgType == "image":
         try:
             message_content = line_bot_api.get_message_content(event['message']['id'])
